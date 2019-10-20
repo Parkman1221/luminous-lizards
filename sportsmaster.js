09358a2002b2008c43a316b7b65f1e7faad7cab5
Parkman1221 <56796143+Parkman1221@users.noreply.github.com>
2019-10-20 11:40:43 -0400

    Update sportsmaster.js

--- sportsmaster.js
+++ sportsmaster.js
@@ -1,8 +1,15 @@
 //Jacob Park
 //Sports Master
 //Spec: Uses Twilio to communicate between players of sports teams
-var accountSid = 'ACbf44c98eabbf9f2c4df28d7d6ea6aa83'; // Your Account SID from www.twilio.com/console
-var authToken = 'b16ed4e28b5100392d0522e5a828a571';   // Your Auth Token from www.twilio.com/console
+/*Instructions: 
+	*Install Twilio Node Library: npm install twilio
+	*Install Express: npm install express
+	*Set up Ngrok on Port 3000: ngrok http 3000
+	*Configure Webhook URL provided by Ngrok on Twilio's website Configuration settings
+*/
+
+var accountSid = 'ACbf44c98eabbf9f2c4df28d7d6ea6aa83';
+var authToken = 'b16ed4e28b5100392d0522e5a828a571';
 
 var twilio = require('twilio');
 var client = new twilio(accountSid, authToken);
@@ -16,13 +23,13 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var teams = [];//array of team object
+var teams = [];//array of team objects
 
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
 
 var inputs = req.body.Body.split(" ");
-if(inputs[0] == "master")//master: master <winningTeam>
+if(inputs[0] == "master")//master input: master <winningTeam>
 {
 	var team0 = teams[0];
 	var team1 = teams[1];
@@ -55,13 +62,13 @@ if(inputs[0] == "master")//master: master <winningTeam>
 	}
 	else
 		console.log("error");
-
+	//notifying team members
 	for(var k = 0; k < teams[0].contactInfo.length; k++)
 	{
 		client.messages.create({
 	    body: 'Team ' + teams[0].teamName + ', Your Team is Ready!',
-	    to: teams[0].contactInfo[k],  // Text this number
-	    from: '+17402004652' // From a valid Twilio number
+	    to: teams[0].contactInfo[k],
+	    from: '+17402004652'
 		})
 		.then((message) => console.log(message.sid));
 	}
@@ -69,8 +76,8 @@ if(inputs[0] == "master")//master: master <winningTeam>
 	{
 		client.messages.create({
 	    body: 'Team ' + teams[1].teamName + ', Your Team is Ready!',
-	    to: teams[0].contactInfo[j],  // Text this number
-	    from: '+17402004652' // From a valid Twilio number
+	    to: teams[0].contactInfo[j],
+	    from: '+17402004652'
 		})
 		.then((message) => console.log(message.sid));
 	}
@@ -92,8 +99,8 @@ else
 
 		client.messages.create({
 	    body: 'You have joined Team ' + teams[check1].teamName,
-	    to: number,  // Text this number
-	    from: '+17402004652' // From a valid Twilio number
+	    to: number,
+	    from: '+17402004652'
 		})
 		.then((message) => console.log(message.sid));
   	}
@@ -109,8 +116,8 @@ else
 
 	client.messages.create({
     body: 'Team ' + thisTeam.teamName + ', Your Team is Queued!',
-    to: thisTeam.contactInfo[0],  // Text this number
-    from: '+17402004652' // From a valid Twilio number
+    to: thisTeam.contactInfo[0],
+    from: '+17402004652'
 	})
 	.then((message) => console.log(message.sid));
   }
@@ -128,4 +135,4 @@ else
 
 http.createServer(app).listen(3000, () => {
   console.log('Express server listening on port 3000');
-});
+}); 
